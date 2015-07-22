c763aca66ef55cff13a664e587b043b31d00d9ed
todesking <discommunicative@gmail.com>
2015-07-22 17:31:43 +0900

    ReduceNatExp: semi-auto prove

--- prove.scala/src/main/scala/main.scala
+++ prove.scala/src/main/scala/main.scala
@@ -71,25 +71,20 @@ object Main {
       import GenericExp._
       import ReduceNatExp._
       Seq(
-        prove[(E_0 + E_2) -*-> E_2](`MR-One`(`R-Plus`(prove[Plus[_0, _2, _2]]))),
-        prove[((E_1 * E_1) + (E_1 * E_1)) `-d->` (E_1 + (E_1 * E_1))](
-          `DR-PlusL`(`DR-Times`(prove[Times[_1, _1, _1]]))),
+        prove[(E_0 + E_2) -*-> E_2],
+        prove[((E_1 * E_1) + (E_1 * E_1)) `-d->` (E_1 + (E_1 * E_1))],
         prove[((E_1 * E_1) + (E_1 * E_1)) `--->` ((E_1 * E_1) + E_1)](
-          `R-PlusR`(`R-Times`(prove[Times[_1, _1, _1]]))),
+          `R-PlusR`(prove[(E_1 * E_1) ---> E_1])), // -*-> is nondeterministic
         prove[((E_1 * E_1) + (E_1 * E_1)) `-*->` E_2](
           `MR-Multi`(
             `MR-Multi`(
               prove[((E_1 * E_1) + (E_1 * E_1)) `-*->` (E_1 + (E_1 * E_1))](
-                `MR-One`(
-                  prove[((E_1 * E_1) + (E_1 * E_1)) ---> (E_1 + (E_1 * E_1))](
-                    `R-PlusL`[(E_1 * E_1), (E_1 * E_1), E_1](`R-Times`(prove[Times[_1, _1, _1]]))))
-              ),
+                `MR-One`(`R-PlusL`(prove[(E_1 * E_1) ---> E_1]))),
               prove[(E_1 + (E_1 * E_1)) `-*->` (E_1 + E_1)](
-                `MR-One`(`R-PlusR`[E_1, E_1 * E_1, E_1](`R-Times`(prove[Times[_1, _1, _1]])))
+                `MR-One`(`R-PlusR`(prove[(E_1 * E_1) ---> E_1]))
               )
             ),
-            prove[(E_1 + E_1) -*-> E_2](
-              `MR-One`(`R-Plus`(prove[Plus[_1, _1, _2]])))
+            prove[(E_1 + E_1) -*-> E_2]
           )
         )
       )
@@ -360,10 +355,10 @@ object ReduceNatExp extends Nat {
     override def str = s"${repr[A]} -d-> ${repr[B]}"
   }
 
-  def `R-Plus`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](ev1: Plus[N1, N2, N3]): (ENum[N1] + ENum[N2]) ---> ENum[N3] =
+  implicit def `R-Plus`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](implicit ev1: Plus[N1, N2, N3]): (ENum[N1] + ENum[N2]) ---> ENum[N3] =
     new --->[ENum[N1] + ENum[N2], ENum[N3]] { override def provenBy = "R-Plus"; override def assumptions = Seq(ev1) }
 
-  def `R-Times`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](ev1: Times[N1, N2, N3]): (ENum[N1] * ENum[N2]) ---> ENum[N3] =
+  implicit def `R-Times`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](implicit ev1: Times[N1, N2, N3]): (ENum[N1] * ENum[N2]) ---> ENum[N3] =
     new --->[ENum[N1] * ENum[N2], ENum[N3]] { override def provenBy = "R-Times"; override def assumptions = Seq(ev1) }
 
   def `R-PlusL`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E1'` <: Exp: Repr](ev1: E1 ---> `E1'`): (E1 + E2) ---> (`E1'` + E2) =
@@ -378,22 +373,22 @@ object ReduceNatExp extends Nat {
   def `R-TimesR`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E2'` <: Exp: Repr](ev1: E2 ---> `E2'`): (E1 * E2) ---> (E1 * `E2'`) =
     new --->[E1 * E2, E1 * `E2'`] { override def provenBy = "R-TimesR"; override def assumptions = Seq(ev1) }
 
-  def `DR-Plus`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](ev1: Plus[N1, N2, N3]): (ENum[N1] + ENum[N2]) `-d->` ENum[N3] =
+  implicit def `DR-Plus`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](implicit ev1: Plus[N1, N2, N3]): (ENum[N1] + ENum[N2]) `-d->` ENum[N3] =
     new `-d->`[ENum[N1] + ENum[N2], ENum[N3]] { override def provenBy = "DR-Plus"; override def assumptions = Seq(ev1) }
 
-  def `DR-Times`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](ev1: Times[N1, N2, N3]): (ENum[N1] * ENum[N2]) `-d->` ENum[N3] =
+  implicit def `DR-Times`[N1 <: Num: Repr, N2 <: Num: Repr, N3 <: Num: Repr](implicit ev1: Times[N1, N2, N3]): (ENum[N1] * ENum[N2]) `-d->` ENum[N3] =
     new `-d->`[ENum[N1] * ENum[N2], ENum[N3]] { override def provenBy = "DR-Times"; override def assumptions = Seq(ev1) }
 
-  def `DR-PlusL`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E1'` <: Exp: Repr](ev1: E1 `-d->` `E1'`): (E1 + E2) `-d->` (`E1'` + E2) =
+  implicit def `DR-PlusL`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E1'` <: Exp: Repr](implicit ev1: E1 `-d->` `E1'`): (E1 + E2) `-d->` (`E1'` + E2) =
     new `-d->`[E1 + E2, `E1'` + E2] { override def provenBy = "DR-PlusL"; override def assumptions = Seq(ev1) }
 
-  def `DR-PlusR`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E2'` <: Exp: Repr](ev1: E2 `-d->` `E2'`): (E1 + E2) `-d->` (E1 + `E2'`) =
+  implicit def `DR-PlusR`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E2'` <: Exp: Repr](implicit ev1: E2 `-d->` `E2'`): (E1 + E2) `-d->` (E1 + `E2'`) =
     new `-d->`[E1 + E2, E1 + `E2'`] { override def provenBy = "DR-PlusR"; override def assumptions = Seq(ev1) }
 
-  def `DR-TimesL`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E1'` <: Exp: Repr](ev1: E1 `-d->` `E1'`): (E1 * E2) `-d->` (`E1'` * E2) =
+  implicit def `DR-TimesL`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E1'` <: Exp: Repr](implicit ev1: E1 `-d->` `E1'`): (E1 * E2) `-d->` (`E1'` * E2) =
     new `-d->`[E1 * E2, `E1'` * E2] { override def provenBy = "DR-TimesL"; override def assumptions = Seq(ev1) }
 
-  def `DR-TimesR`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E2'` <: Exp: Repr](ev1: E2 `-d->` `E2'`): (E1 * E2) `-d->` (E1 * `E2'`) =
+  implicit def `DR-TimesR`[E1 <: Exp: Repr, E2 <: Exp: Repr, `E2'` <: Exp: Repr](implicit ev1: E2 `-d->` `E2'`): (E1 * E2) `-d->` (E1 * `E2'`) =
     new `-d->`[E1 * E2, E1 * `E2'`] { override def provenBy = "DR-TimesR"; override def assumptions = Seq(ev1) }
 
   def `MR-Zero`[E <: Exp: Repr]: E -*-> E =
@@ -402,6 +397,12 @@ object ReduceNatExp extends Nat {
   def `MR-Multi`[E0 <: Exp: Repr, E1 <: Exp: Repr, E2 <: Exp: Repr](ev1: E0 -*-> E1, ev2: E1 -*-> E2): E0 -*-> E2 =
     new -*->[E0, E2] { override def provenBy = "MR-Multi"; override def assumptions = Seq(ev1, ev2) }
 
-  def `MR-One`[E0 <: Exp: Repr, E1 <: Exp: Repr](ev1: E0 ---> E1) : E0 -*-> E1 =
+  def `MR-One`[E0 <: Exp: Repr, E1 <: Exp: Repr](ev1: E0 ---> E1) : E0 -*-> E1 = {
+    implicit val e1 = ev1
+    `MR-One`[E0, E1]
+  }
+  implicit def `MR-One`[E0 <: Exp: Repr, E1 <: Exp: Repr](implicit ev1: E0 ---> E1) : E0 -*-> E1 =
     new -*->[E0, E1] { override def provenBy = "MR-One"; override def assumptions = Seq(ev1) }
 }
+
+
