bd72b28c2eaf5d973f88bbd0832b3569ef00d3d8
todesking <discommunicative@gmail.com>
2015-10-02 22:59:20 +0900

    aaa

--- snd/src/main/scala/main.scala
+++ snd/src/main/scala/main.scala
@@ -3,8 +3,35 @@ import scalaz.Arrow
 import scalaz.syntax.arrow._
 import scala.specialized
 
+trait Simplify[+A, -B] {
+  def build(b: B): A
+}
+
+object Simplify {
+  implicit val evUnit0: Simplify[Unit, Unit] = new Simplify[Unit, Unit] {
+    override def build(u: Unit): Unit = u
+  }
+
+  implicit def evUnitTuple2[A, B](implicit ev1: Simplify[A, Unit], ev2: Simplify[B, Unit]): Simplify[(A, B), Unit] =
+    new Simplify[(A, B), Unit] {
+      override def build(u: Unit): (A, B) = (ev1.build(u) -> ev2.build(u))
+    }
+}
+
 sealed trait SignalFunction[-A, +B] {
   def buildProc(): A => B
+
+  def shrink()(implicit ev: Simplify[A, Unit]): SignalFunction[Unit, B] =
+    SignalFunction.const(()).mapsnd { u => ev.build(u) } >>> this
+
+  def merge[C, D, E](rhs: SignalFunction[C, D])(f: (B, D) => E): SignalFunction[(A, C), E] =
+    (this *** rhs).mapsnd(f.tupled)
+
+  def +[C, D](rhs: SignalFunction[C, D])(implicit num: Numeric[D], evB: B <:< D): SignalFunction[(A, C), D] =
+    this.merge(rhs) { (d1, d2) => num.plus(evB(d1), d2) }
+
+  def *[C, D](rhs: SignalFunction[C, D])(implicit num: Numeric[D], evB: B <:< D): SignalFunction[(A, C), D] =
+    this.merge(rhs) { (d1, d2) => num.times(evB(d1), d2) }
 }
 
 case class FunctionSF[A, B](f: A => B) extends SignalFunction[A, B] {
@@ -52,13 +79,16 @@ object SignalFunction {
       ComposeSF[A, B, C](sf1, sf2)
   }
 
+  implicit def sndInt2Double[A](sf: SignalFunction[A, Int]): SignalFunction[A, Double] =
+    sf >>> SignalFunction(_.toDouble)
+
   def apply[A, B](f: A => B): FunctionSF[A, B] =
     FunctionSF[A, B](f)
 
   def stateful[A, B, @specialized(Int, Double) C](init: C)(f: (A, C) => (B, C)): StatefulSF[A, B, C] =
     StatefulSF(FunctionSF[(A, C), (B, C)](_ match { case (a, c) => f(a, c) }), init)
 
-  val ignore: ConstSF[Unit] = ConstSF(())
+  val unit: ConstSF[Unit] = ConstSF(())
 
   def const[A](value: A): ConstSF[A] = ConstSF(value)
 }
@@ -68,19 +98,30 @@ case class MidiShortMessage(command: Int, channel: Int, data1: Int, data2: Int) 
 
 case class SamplingRate(value: Int) extends AnyVal
 
+object Primitive {
+  // NOTE: phase is normalized(0.0 to 1.0)
+  def freqToPhase(implicit sr: SamplingRate): SignalFunction[Double, Double] =
+    SignalFunction.stateful[Double, Double, Double](0.0) { (freq, state) =>
+      val phase = (state + freq / sr.value)
+      (phase -> phase)
+    }
+}
+
 object VCO {
-  def saw(implicit sr: SamplingRate) = SignalFunction.stateful[Double, Double, Double](0.0) { (freq, state) =>
-    val phase = (state + freq / sr.value) % 1.0
-    val value = phase * 2 - 1.0
-    (value -> phase)
-  }
+  import Primitive._
+
+  def saw(implicit sr: SamplingRate): SignalFunction[Double, Double] =
+    freqToPhase >>> SignalFunction { phase => phase * 2 - 1.0 }
+
+  def sin(implicit sr: SamplingRate): SignalFunction[Double, Double] =
+    freqToPhase >>> SignalFunction { phase => math.sin(phase * 2 * math.Pi) }
 }
 
 class World(samplingRate: SamplingRate) {
-  def run[A](init: A)(genSF: SamplingRate => SignalFunction[A, Double]): Unit = {
+  def runUnit[A](genSF: SamplingRate => SignalFunction[A, Double])(implicit simplify: Simplify[A, Unit]): Unit = {
     val sf = genSF(samplingRate)
     println(s"Running ${sf}...")
-    run(init, sf)
+    run[Unit]((), SignalFunction.unit >>> SignalFunction(simplify.build) >>> sf)
   }
 
   private[this] def run[A](init: A, sf: SignalFunction[A, Double]): Unit = {
@@ -104,6 +145,7 @@ object World {
 
 object JavaSound {
   import javax.sound.sampled.{ AudioSystem, SourceDataLine, AudioFormat, DataLine }
+  import javax.sound.midi.{ MidiSystem, MidiDevice }
 
   def openLine(samplingRate: Int, bufferSizeMS: Int): (SourceDataLine, Int) = {
     import javax.sound.sampled._
@@ -123,13 +165,31 @@ object JavaSound {
     line.start()
     (line, bufferSizeByte)
   }
+
+  def findMidiDevice(name: String, vendor: String): Option[MidiDevice] = {
+    MidiSystem.getMidiDeviceInfo()
+      .find { di => di.getName == name && di.getVendor == vendor }
+      .map { MidiSystem.getMidiDevice(_) }
+  }
+
+  def findNanoKontrol(): Option[(MidiDevice, MidiDevice)] = {
+    val korg = "KORG INC."
+    for {
+      in <- findMidiDevice("SLIDER/KNOB", korg)
+      out <- findMidiDevice("CTRL", korg)
+    } yield (in -> out)
+  }
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
     val w = World.create(SamplingRate(44100))
-    w.run[Unit](()) { implicit sr =>
-      SignalFunction.const(440.0) >>> VCO.saw
+    // val Some((in, out)) = JavaSound.findNanoKontrol()
+
+    import SignalFunction.{ const }
+
+    w.runUnit { implicit sr =>
+      (const(440.0) + (const(2.0) >>> VCO.sin) * const(20.0)) >>> VCO.saw
     }
   }
 }
