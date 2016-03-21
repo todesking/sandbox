b97e5af1df6b92a418a39ff2c2a0a908f857b42e
todesking <discommunicative@gmail.com>
2016-03-22 07:19:28 +0900

    move to todesking/unveil

--- opt/src/main/scala/Frame.scala
+++
@@ -1,9 +0,0 @@
-package com.todesking.unveil
-
-case class Frame(locals: Map[Int, FrameItem], stack: List[FrameItem], effect: Effect) {
-  def local(n: Int): FrameItem =
-    locals(n)
-
-  def stackTop: FrameItem = stack.head
-}
-
