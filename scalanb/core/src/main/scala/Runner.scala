b7b712238067c36b8347d1a40c09844cfc844c40
todesking <discommunicative@gmail.com>
2018-08-15 04:10:09 +0900

    refactor

--- scalanb/core/src/main/scala/Runner.scala
+++ scalanb/core/src/main/scala/Runner.scala
@@ -57,20 +57,4 @@ object Runner {
 
     nakedOut.println(s"Notebook log saved to ${out.path}")
   }
-
-  // TODO: Support ALL methods
-  abstract class TappedPrintStream(original: java.io.PrintStream) extends java.io.PrintStream(original) {
-    override def println(s: String) =
-      print(s + "\n")
-    override def print(s: String) = {
-      tap(s)
-      original.print(s)
-    }
-    protected def tap(s: String): Unit
-  }
-  object TappedPrintStream {
-    def apply(o: java.io.PrintStream)(f: String => Unit): TappedPrintStream = new TappedPrintStream(o) {
-      override def tap(s: String) = f(s)
-    }
-  }
 }
