6e15e90fda73882d25778c61f2aee0aae63d8b05
todesking <discommunicative@gmail.com>
2018-08-17 22:38:49 +0900

    refactor

--- scalanb/spark/src/main/scala/Notebook.scala
+++ scalanb/spark/src/main/scala/Notebook.scala
@@ -1,9 +1,34 @@
 package com.todesking.scalanb.spark
 
-import scala.language.experimental.macros
+import com.todesking.scalanb
+
 import scala.annotation.StaticAnnotation
+import scala.reflect.macros.whitebox.Context
+
+import scala.language.experimental.macros
 
 class Notebook extends StaticAnnotation {
-  def macroTransform(annottees: Any*): Any = macro MacroImpl.notebook.apply
+  def macroTransform(annottees: Any*): Any = macro Notebook.macroImpl.apply
+}
+object Notebook {
+  object macroImpl extends scalanb.Notebook.MacroImpl {
+    override def makeRunMethod(c: Context)(stats: Seq[c.universe.Tree]): c.universe.Tree = {
+      import c.universe._
+      q"""
+          def scalanb__run(scalanb__builder: _root_.com.todesking.scalanb.Builder, spark: _root_.org.apache.spark.sql.SparkSession): _root_.scala.Unit = {
+              ..$stats
+          }"""
+    }
+
+    override def makeMain(c: Context)(tpname: c.universe.TypeName, notebookName: String): c.universe.Tree = {
+      import c.universe._
+      q"""
+      def main(args: Array[String]): Unit = {
+        val target = new ${tpname}()
+        _root_.com.todesking.scalanb.spark.Runner.runBatch(args, target, $notebookName)
+      }
+      """
+    }
+  }
 }
 
