bcdfafa70a7bf97a7d1e62b7ddf7cdf876000956
todesking <discommunicative@gmail.com>
2019-08-11 03:29:36 +0900

    a

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -13,9 +13,9 @@ typedef long long ll;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 
+// debug(...) {{{
 bool debug_enabled = false;
 
-// {{{
 void debug1(char const * x) { cout << x; }
 template<class T> void debug1(const T& x) { cout << x; }
 template<class T1, class T2> void debug1(const pair<T1, T2>& x) { cout << "(" << x.first << ", " << x.second << ")"; }
@@ -39,18 +39,21 @@ template<class T, class... Ts> void debug2(const T& x, const Ts&... xs) {
 
 template<class T, class... Ts> void debug(const T& x, const Ts&... xs) {
   if(debug_enabled) {
+	cout << "[D] ";
 	debug1(x);
 	debug2(xs...);
   }
 }
 // }}}
 
+// main {{{
 void solve();
 int main() {
   auto v = getenv("DEBUG");
   if(v && string(v) == "1") debug_enabled = true;
   solve();
 }
+// }}}
 
 void solve() {
 }
