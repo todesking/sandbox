77941f15958fc612bf6cca1a66f0041e4343ccc2
todesking <discommunicative@gmail.com>
2021-12-10 23:34:19 +0900

    fix vco freq control

--- rustsynth/src/lib.rs
+++ rustsynth/src/lib.rs
@@ -17,14 +17,15 @@ pub struct VCO<R: Rack> {
     // range: 0.0 - 1.0 (0.1Hz - 20.0KHz)
     in_freq: Box<dyn Fn(&R, &R::Input) -> f32 + std::marker::Send>,
     phase: f32,
+    freq_min: f32,
+    freq_max: f32,
     pub out: f32,
 }
 impl<R: Rack> Module<R> for VCO<R> {
     fn update(&mut self, rack: &R, input: &R::Input) {
         let in_freq = (self.in_freq)(rack, input);
-        let a = (20_000.0f32 - 0.1 + 1.0).ln();
         let pi2 = std::f32::consts::PI * 2.0;
-        let freq = 0.1 + (in_freq * a).exp() - 1.0;
+        let freq = (self.freq_min.ln() + in_freq * (self.freq_max.ln() - self.freq_min.ln())).exp();
         self.phase += freq * pi2 / SAMPLES_PER_SEC as f32;
         self.phase %= pi2;
         self.out = (self.phase).sin();
@@ -47,6 +48,8 @@ pub fn new_my_rack() -> MyRack<Input> {
         vco1: RefCell::new(VCO {
             _rack: PhantomData,
             in_freq: Box::new(|_, input| input.slider1 as f32),
+            freq_min: 200.0,
+            freq_max: 15000.0,
             phase: 0.0,
             out: 0.0,
         }),
